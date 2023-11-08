9b69ae098cc01bb061d22d492990a5d28ed1c87a
Nastya07s <nastya.kotovich07@gmail.com>
2023-11-08 19:45:53 +0100

    refactor: remove extra logs

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,18 +3,6 @@ import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-const loadedEnvVariables = Object.keys(process.env).map((key) => ({
-  key,
-  value: process.env[key],
-}));
-
-console.log('Loaded Environment Variables:');
-loadedEnvVariables.forEach((envVar) => {
-  console.log(`${envVar.key}: ${envVar.value}`);
-});
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
